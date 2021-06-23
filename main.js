bbf18d3a2cf7c112e15ee0391a1ea65714e6c716
zhangshun <zhangshun@growingio.com>
2021-06-23 22:06:03 +0800

    feat: redux-saga和store的连接demo

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga';
 
 import Counter from './Counter'
 import reducer from './reducers'
+import { helloSaga } from './saga';
 
-const store = createStore(reducer)
+const helloSageMiddleware = createSagaMiddleware(helloSaga);
+const store = createStore(reducer, applyMiddleware(helloSageMiddleware))
+
+helloSageMiddleware.run(helloSaga);
 
 const action = type => store.dispatch({type})
 
